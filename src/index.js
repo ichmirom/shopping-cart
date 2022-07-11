142c4fdabd4f9f16b70d9583abae978f3f5ad01b
Roman Burkeev <raburkeev@gmail.com>
2022-07-12 01:16:54 +0300

    add props reset etc.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css'
-
-import Counter from './components/counter'
-
-const App = () => {
-  return <Counter/>
-}
+import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
