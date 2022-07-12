fce5cce6193e541e1136d59c8aea19b8793b727c
Roman Burkeev <raburkeev@gmail.com>
2022-07-13 00:36:49 +0300

    add handleIncrement and handleDecrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 
 const Counter = (props) => {
     const {value} = props
@@ -7,28 +7,18 @@ const Counter = (props) => {
         return value === 0 ? 'empty' : value
     }
 
-    const getGabeClasses = () => {
+    const getBadgeClasses = () => {
         let classes = 'badge m-2 '
         classes += value === 0 ? 'bg-warning' : 'bg-primary'
         return classes
     }
 
-    const handleIncrement = () => {
-        console.log(1)
-        // setValue((prevState) => prevState + 1)
-    }
-
-    const handleDecrement = () => {
-        console.log(2)
-        // setValue((prevState) => prevState - 1)
-    }
-
     return (
         <div>
             <span>{props.name}</span>
-            <span className={getGabeClasses()}>{formatCount()}</span>
-            <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
-            <button className='btn btn-primary btn-sm m-2' onClick={handleDecrement}>-</button>
+            <span className={getBadgeClasses()}>{formatCount()}</span>
+            <button className='btn btn-primary btn-sm m-2' onClick={() => props.onIncrement(props.id)}>+</button>
+            <button className='btn btn-primary btn-sm m-2' onClick={() => props.onDecrement(props.id)}>-</button>
             <button className="btn btn-danger btn-sm m-2" onClick={() => props.onDelete(props.id)}>Delete</button>
         </div>
     )
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -20,22 +20,26 @@ const CountersList = () => {
         setCounters(initialState)
     }
 
-    const handleUpdate = () => {
-        const updatedState = [
-            {id: 0, value: 1, name: 'Ненужная вещь'},
-            {id: 1, value: 2, name: 'Ложка'},
-            {id: 2, value: 3, name: 'Вилка'},
-            {id: 3, value: 4, name: 'Тарелка'},
-            {id: 4, value: 0, name: 'Набор минималиста'}
-        ]
+    const handleIncrement = (id) => {
+        const updatedState = [...counters]
+        updatedState[id].value++
+        setCounters(updatedState)
+    }
+
+    const handleDecrement = (id) => {
+        const updatedState = [...counters]
+        updatedState[id].value--
         setCounters(updatedState)
     }
 
     return (
         <>
-            {counters.map(count => <Counter key={count.id} {...count} onDelete={handleDelete}/>)}
+            {counters.map(count => <Counter key={count.id}
+                                            {...count}
+                                            onDelete={handleDelete}
+                                            onIncrement={handleIncrement}
+                                            onDecrement={handleDecrement}/>)}
             <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>Сброс</button>
-            <button className="btn btn-primary btn-sm m-2" onClick={handleUpdate}>Обновить состояние</button>
         </>
     )
 }
